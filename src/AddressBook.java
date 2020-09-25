efac7ab2a7a0914d60487993aeaba17aba694072
santhosh <santhoshpradeepan@cmail.carleton.ca>
2020-09-25 01:25:22 -0400

    creating main method

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -5,15 +5,23 @@ public class AddressBook {
 
     public AddressBook(){
         buddyInfos = new ArrayList<>();
-        System.out.println("Address Book");
+        //System.out.println("Address Book");
     }
 
     public void addBuddy(BuddyInfo buddy) {
         buddyInfos.add(buddy);
     }
 
-    public BuddyInfo removeBuddy() {
-        return null;
+    public void removeBuddy(BuddyInfo buddy) {
+        buddyInfos.remove(buddy);
+    }
+
+    public static void main(String[] args){
+        //System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 
 
